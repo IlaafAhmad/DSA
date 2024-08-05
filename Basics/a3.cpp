80f3a52867a03450aae795872ff555674c352622
Ilaaf Ahmad <ilaafahmad1478@gmail.com>
2024-08-05 11:38:42 +0530

    first commit

---
+++ Basics/a3.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // int, long, long long, float, double
+    // string and getline
+    // char
+
+    string s1, s2;
+    cout << "value of strings are:";  // double quotations for string
+    cin >> s1 >> s2;
+    cout << s1 << " " << s2;
+
+    return 0;
+}
---
+++ Basics/hw.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x;
+    cout << "Value of x is: ";
+    cin >> x;
+    cout << "hey" << x;
+}
