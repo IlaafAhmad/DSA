753a106b2cd6b90c573715a98f53594924453d43
Ilaaf Ahmad <ilaafahmad1478@gmail.com>
2024-08-05 14:24:24 +0530

    Commit

--- Basics/a1.cpp
+++ Basics/a1.cpp
@@ -1,9 +1,7 @@
-# include <bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-    int x, y;
-    cin >> x >> y;
-    cout << "Value of x and y is : " << x << y;
+
     return 0;
 }
